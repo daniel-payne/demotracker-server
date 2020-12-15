cabc7271dbdbe6a578d5c011f288f29e42eb0c16
Daniel <daniel.payne@keldan.co.uk>
2020-12-15 22:43:09 +0000

    Improved marker speed

--- sql/MARKERS_FOR_COUNTRY.sql
+++ sql/MARKERS_FOR_COUNTRY.sql
@@ -1,16 +1,14 @@
 SELECT
-  CAST(round(cast(GT.latitude  as numeric),    :ACCURACY ) AS text) 
+  CAST(round(cast(E.latitude  as numeric),    :ACCURACY ) AS text) 
   || '/' 
-  || CAST(round(cast(GT.longitude as numeric), :ACCURACY ) AS text)                  AS "id",
-  round(cast(GT.latitude  as numeric),         :ACCURACY )                           AS "latitude",
-  round(cast(GT.longitude as numeric),         :ACCURACY )                           AS "longitude",
-  count(*)                                                                           AS "eventCount"
+  || CAST(round(cast(E.longitude as numeric), :ACCURACY ) AS text)                  AS "id",
+  round(cast(E.latitude  as numeric),         :ACCURACY )                           AS "latitude",
+  round(cast(E.longitude as numeric),         :ACCURACY )                           AS "longitude",
+  count(*)                                                                          AS "eventCount"
 FROM
-  global_terrorism_database_2018 GT
-INNER JOIN
-  public.ne_110m_admin_0_countries CO ON ST_Contains(CO.geom, ST_SetSRID(ST_MakePoint(GT.longitude, GT.latitude), 4326))
+  info.events E
 WHERE  
-  CO.fid = :ID
+  E.country_id = :ID
 GROUP BY
-  round(cast(GT.latitude  as numeric),         :ACCURACY ),
-  round(cast(GT.longitude as numeric),         :ACCURACY );
+  round(cast(E.latitude  as numeric),         :ACCURACY ),
+  round(cast(E.longitude as numeric),         :ACCURACY );
