523723b0306cda156a4c73db01f50417a4c7cd6f
Daniel <daniel.payne@keldan.co.uk>
2021-02-09 10:05:57 +0000

    secure cookies

--- data/mutations/login.js
+++ data/mutations/login.js
@@ -1,7 +1,7 @@
 const login = async (parent, args, context, info) => {
   if (args.username === process.env.SESSIONUSER && args.password === process.env.SESSIONPASSWORD) {
     const session = Math.random().toString().substring(2)
-    context.setCookie('session', session)
+    context.setCookie('session', session, { sameSite: 'none', secure: true })
 
     return {
       id: 100,
